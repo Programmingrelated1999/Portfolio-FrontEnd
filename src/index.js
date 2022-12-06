979d0c089d61dbba15ea526691511379d3a70104
Kaung Htet Myat <programmingrelated1999@gmail.com>
2022-12-05 23:05:07 -0500

    added store for index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+import { Provider} from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+import allProjectsReducer from './reducers/allProjectsReducer';
+
+//create store
+const store = configureStore({
+  reducer: {
+    allProjects: allProjectsReducer
+  }
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store = {store}>
+        <App />
+      </Provider>
     </ BrowserRouter>
   </React.StrictMode>
 );
